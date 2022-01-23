7e6731a22986930c1e7c28115646a93a2504c258
vaibhav dhiman <princedhiman70@gmail.com>
2022-01-23 16:50:37 +0530

    form added

--- books-fe/src/app/new-book/new-book.component.ts
+++ books-fe/src/app/new-book/new-book.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-new-book',
   templateUrl: './new-book.component.html',
-  styleUrls: ['./new-book.component.css']
+  styleUrls: ['./new-book.component.css'],
 })
 export class NewBookComponent implements OnInit {
+  bookForm = {
+    name: new FormControl(''),
+    author: new FormControl(''),
+    isbn: new FormControl(''),
+    price: new FormControl(''),
+  };
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onSubmit(form: FormGroup) {
+    console.log('submit called here', form.value);
   }
-
 }
